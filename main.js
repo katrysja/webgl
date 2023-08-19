f7ae89ad10cb5f606f65ff7e0c3559cd6bf3c895
katrysja <catherine.slepchenko@gmail.com>
2023-08-19 15:13:41 +0200

    added responsive design;

--- main.js
+++ main.js
@@ -53,6 +53,12 @@ function main() {
   function render(time) {
     time *= 0.001;  // конвертировать время в секунды
     
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+    
     cubes.forEach((cube, ndx) => {
       const speed = 1 + ndx * .1;
       const rot = time * speed;
@@ -60,6 +66,18 @@ function main() {
       cube.rotation.y = rot;
     });
     renderer.render(scene, camera);
+    // check size of device;
+    function resizeRendererToDisplaySize(renderer) {
+      const canvas = renderer.domElement;
+      const width = canvas.clientWidth;
+      const height = canvas.clientHeight;
+      const needResize = canvas.width !== width || canvas.height !== height;
+      if (needResize) {
+        renderer.setSize(width, height, false);
+      }
+      return needResize;
+    }
+    
     requestAnimationFrame(render);
   }
   requestAnimationFrame(render);
